ee2917f3805ed9264b4e853d3ba49347c0a8821c
jdwillett2 <56878729+jdwillett2@users.noreply.github.com>
2020-02-18 15:26:54 -0500

    Uploaded 2 more scripts

---
+++ Copy-AD-Group.ps1
@@ -0,0 +1,6 @@
+﻿$newgroup = Read-Host "New Group Name"
+$oldgroup = Read-Host "Old Group Name"
+$members = Get-ADGroupMember $oldgroup
+
+New-ADGroup -Name $newgroup -GroupCategory Security -GroupScope Universal -Path "OU=Server Local Groups,OU=Server Groups,OU=Servers,OU=Computer Accounts,DC=chp,DC=clarian,DC=org"
+Add-ADGroupMember -Identity $newgroup -Members $members
