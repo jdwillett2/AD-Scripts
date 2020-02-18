c535125d198b1d70b846810726f7b0c797f8d389
jdwillett2 <56878729+jdwillett2@users.noreply.github.com>
2020-02-18 15:06:04 -0500

    Uploaded get-ad-computer-dn-os

---
+++ Get-AD-Computer-DN-OS.ps1
@@ -0,0 +1,17 @@
+﻿Import-Module ActiveDirectory
+$collection = @()
+$list = Read-Host "input file name"
+$computers = Get-Content $list
+ForEach ($computer in $computers)
+{
+
+    $status = @{"Name" = $computer} 
+    $group = Get-ADComputer $computer | select distinguishedName
+    $status["DN"] = $group
+    $OS = Get-ADComputer $computer -properties OperatingSystem | select OperatingSystem
+    $status["OS"] = $OS
+    New-Object -TypeName PSObject -Property $status -OutVariable serverStatus
+    $collection = $collection + $serverStatus
+}
+
+$collection | Export-Csv -LiteralPath .\results.csv -NoTypeInformation
