03105a506caf77fa81eef33a4923e1b54d32dd03
jdwillett2 <56878729+jdwillett2@users.noreply.github.com>
2020-02-19 18:12:40 -0500

    Removed identifying information

--- Copy-AD-Group.ps1
+++ Copy-AD-Group.ps1
@@ -2,5 +2,5 @@
 $oldgroup = Read-Host "Old Group Name"
 $members = Get-ADGroupMember $oldgroup
 
-New-ADGroup -Name $newgroup -GroupCategory Security -GroupScope Universal -Path "OU=Server Local Groups,OU=Server Groups,OU=Servers,OU=Computer Accounts,DC=chp,DC=clarian,DC=org"
+New-ADGroup -Name $newgroup -GroupCategory Security -GroupScope Universal -Path "OU DN HERE"
 Add-ADGroupMember -Identity $newgroup -Members $members
