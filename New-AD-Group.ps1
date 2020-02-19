766876a737e47eb16975b4371625c4cef01a17dd
jdwillett2 <56878729+jdwillett2@users.noreply.github.com>
2020-02-19 18:13:51 -0500

    Remove identifying information

--- New-AD-Group.ps1
+++ New-AD-Group.ps1
@@ -1,3 +1,3 @@
 ﻿$newgroup = Read-Host "New Group Name"
 
-New-ADGroup -Name $newgroup -GroupCategory Security -GroupScope Universal -Path "OU=Server Local Groups,OU=Server Groups,OU=Servers,OU=Computer Accounts,DC=chp,DC=clarian,DC=org"
+New-ADGroup -Name $newgroup -GroupCategory Security -GroupScope Universal -Path "OU DN HERE"
